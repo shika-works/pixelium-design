9a6b704a4093939dad6a945ecfb6ef097fa14829
LittleRangiferTarandus <694408762@qq.com>
2025-10-17 15:08:46 +0800

    fix(doc): remove old code in doc of custom theme

--- packages/document/template/config/theme-basic.vue
+++ packages/document/template/config/theme-basic.vue
@@ -1,34 +1,11 @@
 <template>
-	Current Theme Color: {{ mainColor }}
-	<br />
 	<px-space>
 		<px-button @click="set()">Set Main Color to Blue</px-button>
 		<px-button @click="reset()" theme="info">Reset</px-button>
 	</px-space>
 </template>
 <script setup lang="ts">
-import { ref, onBeforeUnmount } from 'vue'
 import { setThemeColor } from '@pixelium/web-vue'
-
-const mainColor = ref('')
-const getMainColor = () => {
-	if (typeof window === 'undefined') {
-		return
-	}
-	mainColor.value = getComputedStyle(document.documentElement).getPropertyValue(
-		`--px-primary-6`
-	)
-}
-getMainColor()
-if (typeof window !== 'undefined') {
-	const cb = () => {
-		getMainColor()
-	}
-	window.addEventListener('px-global-css-var-change', cb)
-	onBeforeUnmount(() => {
-		window.removeEventListener('px-global-css-var-change', cb)
-	})
-}
 
 const set = () => {
 	setThemeColor('primary', '#409EFF')
