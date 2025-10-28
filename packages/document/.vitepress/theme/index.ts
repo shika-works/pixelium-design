67897df172a64f8435eea54cff70a415ba26930d
LittleRangiferTarandus <694408762@qq.com>
2025-10-29 00:59:40 +0800

    docs: fix error in ssr build

--- packages/document/.vitepress/theme/index.ts
+++ packages/document/.vitepress/theme/index.ts
@@ -14,32 +14,31 @@ import IconExamplePa from '../../template/common/icon-example-pa.vue'
 
 import { defineClientComponentConfig } from '@vitepress-demo-preview/core'
 
-defineClientComponentConfig({
-	copySuccessText: 'Code copied to clipboard!',
-	vueApp: app,
-	i18n: {
-		en: {
-			copySuccessText: 'Code copied to clipboard!',
-			copyCode: 'Copy code',
-			foldCode: 'Fold code',
-			expandCode: 'Expand code',
-			hideSourceCode: 'Hide source code'
-		},
-		zh: {
-			copySuccessText: '代码已复制到剪贴板！',
-			copyCode: '复制代码',
-			foldCode: '折叠代码',
-			expandCode: '展开代码',
-			hideSourceCode: '隐藏源代码'
-		}
-	},
-	defaultLanguage: 'en'
-})
-
 export default {
 	extends: DefaultTheme,
 	Layout,
 	enhanceApp({ app }) {
+		defineClientComponentConfig({
+			copySuccessText: 'Code copied to clipboard!',
+			vueApp: app,
+			i18n: {
+				en: {
+					copySuccessText: 'Code copied to clipboard!',
+					copyCode: 'Copy code',
+					foldCode: 'Fold code',
+					expandCode: 'Expand code',
+					hideSourceCode: 'Hide source code'
+				},
+				zh: {
+					copySuccessText: '代码已复制到剪贴板！',
+					copyCode: '复制代码',
+					foldCode: '折叠代码',
+					expandCode: '展开代码',
+					hideSourceCode: '隐藏源代码'
+				}
+			},
+			defaultLanguage: 'en'
+		})
 		app.use(PixeliumVue)
 		app.component('demo-preview', AntDesignContainer)
 		app.component('IconExamplePa', IconExamplePa)
