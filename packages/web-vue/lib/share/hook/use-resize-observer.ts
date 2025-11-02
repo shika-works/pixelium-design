31d761e840e769202eb0c6af8fe869dbfe8f6198
LittleRangiferTarandus <694408762@qq.com>
2025-11-03 03:48:28 +0800

    feat: use-resize-observer can handle conditionally rendered elements

--- packages/web-vue/lib/share/hook/use-resize-observer.ts
+++ packages/web-vue/lib/share/hook/use-resize-observer.ts
@@ -1,24 +1,61 @@
 import type { Nullish } from 'parsnip-kit'
-import { onBeforeUnmount, onMounted, type Ref, nextTick } from 'vue'
+import {
+	onBeforeUnmount,
+	onMounted,
+	type Ref,
+	nextTick,
+	type ComputedRef,
+	watch,
+	type WatchHandle
+} from 'vue'
 import { inBrowser } from '../util/env'
 
 export const useResizeObserver = (
 	ref: Ref<Nullish | HTMLElement>,
 	callback: () => any,
-	leading?: boolean
+	leading?: boolean,
+	rendered?: ComputedRef<boolean> | Ref<boolean>
 ) => {
 	if (!inBrowser()) {
 		return null
 	}
-	const resizeObserver = new ResizeObserver(callback)
+	let resizeObserver: ResizeObserver | null =
+		rendered && !rendered.value ? null : new ResizeObserver(callback)
 	onMounted(() => {
 		nextTick(() => {
-			leading && callback()
-			ref.value && resizeObserver.observe(ref.value)
+			if ((rendered && !rendered.value) || !resizeObserver) {
+				return
+			}
+			if (leading) {
+				callback()
+			}
+			if (ref.value) {
+				resizeObserver.observe(ref.value)
+			}
 		})
 	})
+
+	let watchReturn: undefined | WatchHandle
+	if (rendered) {
+		watchReturn = watch(rendered, (val) => {
+			if (!val && resizeObserver) {
+				resizeObserver.disconnect()
+				resizeObserver = null
+			} else if (val && !resizeObserver) {
+				resizeObserver = new ResizeObserver(callback)
+				if (ref.value) {
+					resizeObserver.observe(ref.value)
+				}
+			}
+		})
+	}
 	onBeforeUnmount(() => {
-		resizeObserver.disconnect()
+		if (resizeObserver) {
+			resizeObserver.disconnect()
+			resizeObserver = null
+		}
+		if (watchReturn) {
+			watchReturn()
+		}
 	})
-	return resizeObserver
 }
