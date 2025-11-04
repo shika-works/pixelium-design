e2ad33b62be5f5c7bcf5b0d92e0d2e7c07d7a7ea
LittleRangiferTarandus <694408762@qq.com>
2025-11-05 00:42:46 +0800

    docs: update example of button-group

--- packages/document/template/common/button-group.vue
+++ packages/document/template/common/button-group.vue
@@ -18,11 +18,11 @@
 			<px-button color="#E956AE" variant="plain">#E956AE</px-button>
 			<px-button disabled>Loading</px-button>
 		</px-button-group>
-		<px-button-group variant="outline">
+		<px-button-group>
 			<px-button>Primary</px-button>
-			<px-button theme="sakura">Sakura</px-button>
+			<px-button variant="outline" theme="sakura">Sakura</px-button>
 			<px-button color="#E956AE" variant="plain">#E956AE</px-button>
-			<px-button loading>Loading</px-button>
+			<px-button loading variant="text">Loading</px-button>
 		</px-button-group>
 		<px-button-group :border-radius="[20, 16, 20, 16]">
 			<px-button>Primary</px-button>
