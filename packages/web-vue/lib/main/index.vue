a036919cd3161af147d73ad44560b69e04078974
shikakokou <694408762@qq.com>
2025-12-14 14:26:26 +0800

    feat: add soft props to main

--- packages/web-vue/lib/main/index.vue
+++ packages/web-vue/lib/main/index.vue
@@ -1,10 +1,21 @@
 <template>
-	<main class="pixelium px-main">
+	<main
+		class="pixelium px-main"
+		:class="{
+			'px-main__soft': props.soft
+		}"
+	>
 		<slot></slot>
 	</main>
 </template>
 <script setup lang="ts">
+import type { MainProps } from './type'
+
 defineOptions({ name: 'Main' })
+
+const props = withDefaults(defineProps<MainProps>(), {
+	soft: false
+})
 </script>
 <style lang="less" src="./index.less" />
 
--- packages/web-vue/lib/main/type.ts
+++ packages/web-vue/lib/main/type.ts
@@ -1,3 +1,11 @@
+export type MainProps = {
+	/**
+	 * @property {boolean} [soft=false]
+	 * @version 0.0.3
+	 */
+	soft?: boolean
+}
+
 export type MainSlots = {
 	/**
 	 * @slot default
