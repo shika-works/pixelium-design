b9c7588f5823512076b7d9e7178f4adcda02ca23
shikakokou <694408762@qq.com>
2025-12-27 11:51:46 +0800

    fix: incorrect style lang

--- packages/web-vue/lib/alert/index.vue
+++ packages/web-vue/lib/alert/index.vue
@@ -251,4 +251,4 @@ usePolling(
 
 <style lang="less" src="./index.less"></style>
 
-<style lang="less" src="../share/style/index.css" />
+<style src="../share/style/index.css" />
--- packages/web-vue/lib/badge/index.vue
+++ packages/web-vue/lib/badge/index.vue
@@ -157,4 +157,4 @@ usePolling(
 </script>
 
 <style lang="less" src="./index.less"></style>
-<style lang="less" src="../share/style/index.css" />
+<style src="../share/style/index.css" />
