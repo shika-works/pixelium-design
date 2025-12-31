57c5decd5f3987e4c17143392e41028b1305bb05
shikakokou <694408762@qq.com>
2026-01-01 00:34:17 +0800

    docs: update docs of menu

--- packages/document/template/navigation/menu-horizontal-ellipsis.vue
+++ packages/document/template/navigation/menu-horizontal-ellipsis.vue
@@ -1,7 +1,5 @@
 <template>
-	<div>
-		<px-menu direction="horizontal" :ellipsis="true">
-			<px-menu-item :key="i" :index="i" v-for="i in 15">Item {{ i }}</px-menu-item>
-		</px-menu>
-	</div>
+	<px-menu direction="horizontal" :ellipsis="true">
+		<px-menu-item :key="i" :index="i" v-for="i in 15">Item {{ i }}</px-menu-item>
+	</px-menu>
 </template>
