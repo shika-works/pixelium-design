479276d685a6150a909128bd6bd2f17058abb97f
shikakokou <694408762@qq.com>
2026-01-03 00:16:35 +0800

    test: increase max timeout of test case

--- packages/web-vue/script/vite.config.vue.ts
+++ packages/web-vue/script/vite.config.vue.ts
@@ -32,6 +32,7 @@ export default defineConfig({
 	// @ts-ignore
 	test: {
 		environment: 'jsdom',
+		testTimeout: 10 * 1000,
 		coverage: {
 			provider: 'v8',
 			include: ['lib/**/*.{ts,tsx,js,jsx,vue}'],
