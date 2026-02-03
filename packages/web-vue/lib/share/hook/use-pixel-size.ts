0e7ae8de36d5b9709998464da293b6b8587f7b41
shikakokou <694408762@qq.com>
2026-02-03 19:53:50 +0800

    fix: use-pixel-size cannot change correctly when --px-bit change before comp mount

--- packages/web-vue/lib/share/hook/use-pixel-size.ts
+++ packages/web-vue/lib/share/hook/use-pixel-size.ts
@@ -1,11 +1,10 @@
-import { ref, readonly, onUnmounted } from 'vue'
+import { ref, readonly } from 'vue'
 import { calcPixelSize } from '../util/plot'
 import { GLOBAL_CSS_VAR_CHANGE } from '../const/event-bus-key'
 import { EventBus } from '../util/event-bus'
 import { inBrowser } from '../util/env'
 
 const pixelSizeRef = ref(calcPixelSize() || 0)
-let isInitialized = 0
 
 const callback = () => {
 	pixelSizeRef.value = calcPixelSize() || 0
@@ -14,18 +13,8 @@ const callback = () => {
 if (inBrowser()) {
 	window.addEventListener('load', callback)
 }
+EventBus.on(GLOBAL_CSS_VAR_CHANGE, callback)
 
 export const usePixelSize = () => {
-	if (!isInitialized) {
-		isInitialized++
-		EventBus.on(GLOBAL_CSS_VAR_CHANGE, callback)
-	}
-	onUnmounted(() => {
-		isInitialized--
-		if (!isInitialized) {
-			EventBus.off(GLOBAL_CSS_VAR_CHANGE, callback)
-		}
-	})
-
 	return readonly(pixelSizeRef)
 }
