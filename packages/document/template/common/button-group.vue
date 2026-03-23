88660e3ad541ae830ecbaf65c4e2a0a309127d41
shikakokou <694408762@qq.com>
2026-03-23 23:25:33 +0800

    docs(button): align other changes

--- packages/document/template/common/button-group.vue
+++ packages/document/template/common/button-group.vue
@@ -8,27 +8,27 @@
 		</px-button-group>
 		<px-button-group shape="round">
 			<px-button>Primary</px-button>
-			<px-button theme="sakura">Sakura</px-button>
+			<px-button theme="notice">Notice</px-button>
 			<px-button color="#E956AE" variant="plain">#E956AE</px-button>
 			<px-button loading>Loading</px-button>
 		</px-button-group>
 		<px-button-group loading>
-			<px-button>Primary</px-button>
+			<px-button variant="text">Primary</px-button>
 			<px-button theme="sakura">Sakura</px-button>
 			<px-button color="#E956AE" variant="plain">#E956AE</px-button>
 			<px-button disabled>Loading</px-button>
 		</px-button-group>
-		<px-button-group theme="notice">
-			<px-button>First</px-button>
-			<px-button>Second</px-button>
-			<px-button color="#E956AE">Third</px-button>
-			<px-button loading variant="text">Fourth</px-button>
-		</px-button-group>
-		<px-button-group :border-radius="[20, 16, 20, 16]">
+		<px-button-group :border-radius="[24, 12, 24, 12]">
 			<px-button>First</px-button>
 			<px-button>Second</px-button>
 			<px-button>Third</px-button>
 			<px-button>Fourth</px-button>
+		</px-button-group>
+		<px-button-group shape="circle" theme="notice">
+			<px-button>1</px-button>
+			<px-button>2</px-button>
+			<px-button>3</px-button>
+			<px-button loading>4</px-button>
 		</px-button-group>
 	</px-space>
 </template>
--- packages/document/template/common/button-size.vue
+++ packages/document/template/common/button-size.vue
@@ -1,7 +1,17 @@
 <template>
-	<px-space>
-		<px-button size="small">Small</px-button>
-		<px-button theme="success">Default</px-button>
-		<px-button size="large">Large</px-button>
+	<px-space direction="vertical">
+		<px-space>
+			<px-button size="small">Small</px-button>
+			<px-button theme="success">Default</px-button>
+			<px-button size="large">Large</px-button>
+		</px-space>
+		<px-space>
+			<px-button size="small" shape="circle">S</px-button>
+			<px-button size="small" shape="square">S</px-button>
+			<px-button theme="success" shape="circle">M</px-button>
+			<px-button theme="success" shape="square">M</px-button>
+			<px-button size="large" shape="circle">L</px-button>
+			<px-button size="large" shape="square">L</px-button>
+		</px-space>
 	</px-space>
 </template>
