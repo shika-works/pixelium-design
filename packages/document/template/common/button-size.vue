9e64cb174c5e3760afc487296f66f200f99a00bf
shikakokou <694408762@qq.com>
2026-03-24 13:00:12 +0800

    chore: 0.1.2

--- packages/document/template/common/button-size.vue
+++ packages/document/template/common/button-size.vue
@@ -6,12 +6,12 @@
 			<px-button size="large">Large</px-button>
 		</px-space>
 		<px-space>
-			<px-button size="small" shape="circle">S</px-button>
-			<px-button size="small" shape="square">S</px-button>
+			<px-button size="small" theme="notice" shape="circle">S</px-button>
+			<px-button size="small" theme="notice" shape="square">S</px-button>
 			<px-button theme="success" shape="circle">M</px-button>
 			<px-button theme="success" shape="square">M</px-button>
-			<px-button size="large" shape="circle">L</px-button>
-			<px-button size="large" shape="square">L</px-button>
+			<px-button size="large" theme="notice" shape="circle">L</px-button>
+			<px-button size="large" theme="notice" shape="square">L</px-button>
 		</px-space>
 	</px-space>
 </template>
