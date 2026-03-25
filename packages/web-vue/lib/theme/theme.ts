ec5043c8d7037b3aeb72105210d22f02fae46927
shikakokou <694408762@qq.com>
2026-03-25 22:27:05 +0800

    feat: add css var --px-half-bit

--- packages/web-vue/lib/theme/theme.ts
+++ packages/web-vue/lib/theme/theme.ts
@@ -105,7 +105,8 @@ export const calcSizes = (pixelSize: number, borderSize: number) => {
 		'--px-small-compat-size': smallSize - INTERVAL,
 		'--px-large-compat-base-size': largeSubSize - 2 * borderSize,
 		'--px-medium-compat-base-size': mediumSubSize - 2 * borderSize,
-		'--px-small-compat-base-size': smallSubSize - 2 * borderSize
+		'--px-small-compat-base-size': smallSubSize - 2 * borderSize,
+		'--px-half-bit': pixelSize / 2
 	}
 }
 
