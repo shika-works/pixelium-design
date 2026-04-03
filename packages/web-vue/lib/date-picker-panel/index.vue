da76be6314b87b7dad63323e594b2ac4c543fd86
shikakokou <694408762@qq.com>
2026-04-03 23:00:50 +0800

    chore: temp

--- packages/web-vue/lib/date-picker-panel/index.vue
+++ packages/web-vue/lib/date-picker-panel/index.vue
@@ -1,94 +1,147 @@
 <template>
-  <div class="pixelium px-date-picker-panel">
-    <div class="px-date-picker-panel-weeks">
-      <div v-for="week in weekDays" :key="week" class="px-date-picker-panel-week-item">{{ week }}</div>
-    </div>
+	<div class="pixelium px-date-picker-panel">
+		<div class="px-date-picker-panel-header">
+			<div class="px-date-picker-panel-nav" @click="changeYear(-1)">
+				<ArrowLeft class="px-date-picker-panel-nav-icon"></ArrowLeft>
+			</div>
+			<div class="px-date-picker-panel-nav" @click="changeMonth(-1)">
+				<AngleLeft class="px-date-picker-panel-nav-icon"></AngleLeft>
+			</div>
+			<Popover
+				:trigger="'click'"
+				placement="bottom"
+				:arrow="false"
+				root="body"
+				:offset="4"
+				:visible="yearMonthPickerVisible"
+				@update:visible="(value) => (yearMonthPickerVisible = value)"
+			>
+				<template #default>
+					<button class="px-date-picker-panel-title" type="button">
+						{{ year }} 年 {{ month + 1 }} 月
+					</button>
+				</template>
 
-    <div class="px-date-picker-panel-grid">
-      <div
-        v-for="(item, index) in calendarDays"
-        :key="index"
-        :class="[
-          'px-date-picker-panel-day-item',
-          { 'px-date-picker-panel-day-item__not-current': !item.isCurrentMonth },
-          { 'px-date-picker-panel-day-item__today': item.isToday }
-        ]"
-      >
-        <span class="px-date-picker-panel-day-number">{{ item.day }}</span>
-      </div>
-    </div>
-  </div>
+				<template #content>
+					<div class="px-date-picker-panel-dropdown">
+						<ScrollPicker :options="yearOptions" :current="String(year)" @select="selectYear" />
+						<ScrollPicker
+							:options="monthOptions"
+							:current="String(month + 1)"
+							@select="selectMonth"
+						/>
+					</div>
+				</template>
+			</Popover>
+			<div class="px-date-picker-panel-nav" @click="changeMonth(1)">
+				<AngleRight class="px-date-picker-panel-nav-icon"></AngleRight>
+			</div>
+			<div class="px-date-picker-panel-nav" @click="changeYear(1)">
+				<ArrowRight class="px-date-picker-panel-nav-icon"></ArrowRight>
+			</div>
+		</div>
+
+		<DatePickerBody :year="year" :month="month" :current="currentDate" />
+	</div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue'
+import DatePickerBody from '../date-picker-body/index.vue'
+import Popover from '../popover/index.vue'
+import ScrollPicker from '../scroll-picker/index.vue'
+import type { DatePickerPanelEvent, DatePickerPanelProps } from './type'
+
+import AngleLeft from '@hackernoon/pixel-icon-library/icons/SVG/regular/angle-left.svg'
+import ArrowLeft from '@hackernoon/pixel-icon-library/icons/SVG/regular/arrow-left.svg'
+import AngleRight from '@hackernoon/pixel-icon-library/icons/SVG/regular/angle-right.svg'
+import ArrowRight from '@hackernoon/pixel-icon-library/icons/SVG/regular/arrow-right.svg'
 
 defineOptions({
-  name: 'DatePickerPanel'
+	name: 'DatePickerPanel'
 })
 
-interface Props {
-  year: number;
-  month: number;
+const props = withDefaults(defineProps<DatePickerPanelProps>(), {
+	current: undefined
+})
+
+const emits = defineEmits<DatePickerPanelEvent>()
+
+const currentDate = ref<Date>(new Date())
+const yearMonthPickerVisible = ref(false)
+
+onMounted(() => {
+	if (props.current instanceof Date && !Number.isNaN(props.current.getTime())) {
+		currentDate.value = new Date(props.current)
+	}
+})
+
+watch(
+	() => props.current,
+	(next) => {
+		if (next instanceof Date && !Number.isNaN(next.getTime())) {
+			currentDate.value = new Date(next)
+		}
+	}
+)
+
+const year = computed(() => currentDate.value.getFullYear())
+const month = computed(() => currentDate.value.getMonth())
+
+const normalizeDate = (nextDate: Date) => {
+	currentDate.value = nextDate
+	emits('change', new Date(nextDate))
 }
 
-const props = defineProps<Props>();
-
-const weekDays = ['日', '一', '二', '三', '四', '五', '六'];
-
-interface CalendarItem {
-  day: number;
-  month: number;
-  year: number;
-  isCurrentMonth: boolean;
-  isToday: boolean;
+const changeYear = (delta: number) => {
+	const next = new Date(currentDate.value)
+	next.setFullYear(next.getFullYear() + delta)
+	normalizeDate(next)
 }
 
-const calendarDays = computed(() => {
-  const days: CalendarItem[] = [];
-  const { year, month } = props;
+const changeMonth = (delta: number) => {
+	const next = new Date(currentDate.value)
+	next.setMonth(next.getMonth() + delta)
+	normalizeDate(next)
+}
 
-  const firstDayOfWeek = new Date(year, month - 1, 1).getDay();
-  const currentMonthDays = new Date(year, month, 0).getDate();
-  const lastMonthDays = new Date(year, month - 1, 0).getDate();
+const selectYear = (value: string | number) => {
+	const nextYear = Number(value)
+	if (Number.isNaN(nextYear)) {
+		return
+	}
+	const next = new Date(currentDate.value)
+	next.setFullYear(nextYear)
+	normalizeDate(next)
+}
 
-  const today = new Date();
-  const todayStr = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+const selectMonth = (value: string | number) => {
+	const monthIndex = Number(value) - 1
+	if (Number.isNaN(monthIndex) || monthIndex < 0 || monthIndex > 11) {
+		return
+	}
+	const next = new Date(currentDate.value)
+	next.setMonth(monthIndex)
+	normalizeDate(next)
+	yearMonthPickerVisible.value = false
+}
 
-  for (let i = firstDayOfWeek - 1; i >= 0; i--) {
-    days.push({
-      day: lastMonthDays - i,
-      month: month - 1,
-      year: month === 1 ? year - 1 : year,
-      isCurrentMonth: false,
-      isToday: false
-    });
-  }
+const yearOptions = computed(() => {
+	const center = year.value
+	const range = 30
+	const start = center - range
+	const end = center + range
+	const options: string[] = []
+	for (let y = start; y <= end; y++) {
+		options.push(String(y))
+	}
+	return options
+})
 
-  for (let i = 1; i <= currentMonthDays; i++) {
-    const isToday = `${year}-${month}-${i}` === todayStr;
-    days.push({
-      day: i,
-      month,
-      year,
-      isCurrentMonth: true,
-      isToday
-    });
-  }
-
-  const remaining = 42 - days.length;
-  for (let i = 1; i <= remaining; i++) {
-    days.push({
-      day: i,
-      month: month + 1,
-      year: month === 12 ? year + 1 : year,
-      isCurrentMonth: false,
-      isToday: false
-    });
-  }
-
-  return days;
-});
+const monthOptions = computed(() => {
+	return Array.from({ length: 12 }, (_, i) => String(i + 1))
+})
 </script>
 
-<style src="./index.less" lang="less"></style>
+<style src="./index.less" lang="less" />
+<style src="../share/style/index.css" />
